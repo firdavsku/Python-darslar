844829309924e8d92dce4ef7c2285bf2b498fc23
Your Name <firdavsef700@gmail.com>
2023-07-04 10:12:48 +0500

    lessons-ifels

---
+++ dictionary.py
@@ -0,0 +1,29 @@
+
+
+# car_0 = {'model':'ferrari','rang':'qizil','narx':20000}
+# print(car_0['model'])
+# print(car_0['rang'])
+
+
+# talaba_0 = {'ism':'murod olimov','yosh':20,'t_yil':2000}
+# print(talaba_0)
+# del talaba_0['yosh'] # yosh degan kalit so'z (va qiymatni) o'chiramiz
+# print(talaba_0)
+
+# telefonlar = {
+#     'ali':'iphone x',
+#     'vali':'galaxy s9',
+#     'olim':'mi 10 pro',
+#     'orif':'nokia 3310'
+#     }
+
+
+# talaba_1 = {'ism':'murod olimov','yosh':23,'t_yil':2000}
+# print(f"{talaba_1['ism'].title()},\
+#  {talaba_1['t_yil']}-yilda tu'gilgan,\
+#  {talaba_1['yosh']} yoshda")
+
+
+# yil = int(input("Yil = "))
+# if 2099 >= yil >= 2000:
+#     print("XXI asr")
---
+++ if-else.py
@@ -0,0 +1,9 @@
+
+
+# Masala 1
+
+# a = float(input("son kiriting = "))
+# if a>0:
+#     print("musbat")
+# else:
+#     print("Mandiy")
